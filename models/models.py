3a042e23b32669e0534b877972f0058da83307eb
Paulin Gasquet <paulin.gasquet@gmail.com>
2026-04-20 17:40:59 +0200

    feat: create models for Poll, Option, and Vote

---
+++ models/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Poll(Base):
+    __tablename__ = "polls"
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String, nullable=False)
+    closes_at = Column(DateTime, nullable=True) 
+    
+    options = relationship("Option", back_populates="poll", cascade="all, delete-orphan") # le paramètre "cascade" permet de supprimer les options associées lorsqu'un sondage est supprimé
+    votes = relationship("Vote", back_populates="poll", cascade="all, delete-orphan")
+
+class Option(Base):
+    __tablename__ = "options"
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=False)
+    poll_id = Column(Integer, ForeignKey("polls.id"))
+    
+    poll = relationship("Poll", back_populates="options")
+    votes = relationship("Vote", back_populates="option", cascade="all, delete-orphan")
+
+class Vote(Base):
+    __tablename__ = "votes"
+    id = Column(Integer, primary_key=True, index=True)
+    voter_token = Column(String, nullable=False)
+    option_id = Column(Integer, ForeignKey("options.id"))
+    poll_id = Column(Integer, ForeignKey("polls.id"))
+    
+    option = relationship("Option", back_populates="votes")
+    poll = relationship("Poll", back_populates="votes")
